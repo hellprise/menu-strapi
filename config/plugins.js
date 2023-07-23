9b9242d81ee24649b7f4f2daf9e5de8e80a86922
hellprise <mleokha@gmail.com>
2023-07-23 19:44:44 +0300

    check pt.4

--- config/plugins.js
+++ config/plugins.js
@@ -6,6 +6,7 @@ module.exports = ({ env }) => ({
       playgroundAlways: true,
       depthLimit: 7,
       amountLimit: 100,
+      introspection: true,
       apolloServer: {
         tracing: true,
       },
